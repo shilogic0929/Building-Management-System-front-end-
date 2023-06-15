a8364952dbcee5b60282a67a5f00b7f5dc0d9a59
zhy <2045394183@qq.com>
2023-06-15 15:36:45 +0800

    zhy

--- edifice/src/App.vue
+++ edifice/src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view></router-view>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body
+{
+  margin: 0;
+  padding: 0;
+}
+#app
+{
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+  'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+a{text-decoration: none;}
+.el-empty
+{
+  margin-top: 130px;
+}
+.el-dialog
+{
+  border-radius: 8px !important;
+}
+.el-dialog__header
+{
+  font-size: 20px;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
   font-weight: bold;
-  color: #2c3e50;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+ul
+{
+  padding-inline-start: 0 !important;
+  list-style-type: none;
+}
+hr
+{
+  border: none;
+  height: 1px;
+  background-color: rgb(220,223,230);
+}
+::-webkit-scrollbar
+{
+  width: 10px;
+  height: 10px;
+}
+::-webkit-scrollbar-thumb
+{
+  background-color: rgb(167, 159, 159);
+  border-radius: 32px;
+}
+::-webkit-scrollbar-track
+{
+  background-color: transparent;
+  border-radius: 32px;
 }
 </style>
