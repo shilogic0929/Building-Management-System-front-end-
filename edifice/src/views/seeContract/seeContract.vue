d88c673338319088be7aaea5bad31370906af729
rainysong <songyuqwq@163.com>
2023-06-16 16:26:24 +0800

    for pull

--- edifice/src/views/seeContract/seeContract.vue
+++ edifice/src/views/seeContract/seeContract.vue
@@ -157,36 +157,34 @@ export default {
           shop: '王小虎夫妻店',
           shopId: '10333'
                 }],
-                gridData : [
-  {
-    date: '2016-05-02',
-    name: 'John Smith',
-    address: 'No.1518,  Jinshajiang Road, Putuo District',
-  },
-  {
-    date: '2016-05-04',
-    name: 'John Smith',
-    address: 'No.1518,  Jinshajiang Road, Putuo District',
-  },
-  {
-    date: '2016-05-01',
-    name: 'John Smith',
-    address: 'No.1518,  Jinshajiang Road, Putuo District',
-  },
-  {
-    date: '2016-05-03',
-    name: 'John Smith',
-    address: 'No.1518,  Jinshajiang Road, Putuo District',
-  },
-]
+          gridData : [
+            {
+                date: '2016-05-02',
+                name: 'John Smith',
+                address: 'No.1518,  Jinshajiang Road, Putuo District',
+            },
+            {
+                date: '2016-05-04',
+                name: 'John Smith',
+                address: 'No.1518,  Jinshajiang Road, Putuo District',
+            },
+            {
+                date: '2016-05-01',
+                name: 'John Smith',
+                address: 'No.1518,  Jinshajiang Road, Putuo District',
+            },
+            {
+                date: '2016-05-03',
+                name: 'John Smith',
+                address: 'No.1518,  Jinshajiang Road, Putuo District',
+            },
+            ]
     }
   },
   mounted() {
   },
     methods: {
   }
-
-
 }
 </script>
 <style>
