7751a87c10a7c343e2b71a3da1b275f149c3c3f3
z777888xxx <969932827@qq.com>
2023-06-20 11:12:26 +0800

    workInfor1.1

--- edifice/src/views/login/login.vue
+++ edifice/src/views/login/login.vue
@@ -229,16 +229,13 @@ export default {
           // console.log(naid)
           // console.log(res.data.data)
           if(res.data.data.type === 0){//普通人员
-            // this.$router.push('/myRepair')
-            this.$router.push('/woIn')
+            this.$router.push('/dashboard')
           }
           else if(res.data.data.type === -1){//管理人
-            this.$router.push('/woIn')
-            // this.$router.push('/handleRepair')
+            this.$router.push('/handleRepair')
           }
           else{//维修人员
-            this.$router.push('/woIn')
-            // this.$router.push('/handleRepair')
+            this.$router.push('/handleRepair')
           }
         }
       })
