d8291c57d84449455c01403640a4714fc2e01720
梁宇辰 <19376244@buaa.edu.cn>
2023-06-25 10:01:01 +0800

    6.25提交

---
+++ edifice/src/views/knowledgeBase/knowledgeBase.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>维修知识库</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
--- edifice/src/views/roomStatus/roomStatus.vue
+++ edifice/src/views/roomStatus/roomStatus.vue
@@ -1,5 +1,38 @@
 <template>
-  <RoomMap></RoomMap>
+  <div class="container">
+    <div class="left">
+      <RoomMap :thislevel="'1'"></RoomMap>
+      <RoomMap :thislevel="'2'"></RoomMap>
+      <RoomMap :thislevel="'3'"></RoomMap>
+      <RoomMap :thislevel="'4'"></RoomMap>
+      <RoomMap :thislevel="'5'"></RoomMap>
+      <RoomMap :thislevel="'6'"></RoomMap>
+      <RoomMap :thislevel="'7'"></RoomMap>
+      <RoomMap :thislevel="'8'"></RoomMap>
+      <RoomMap :thislevel="'9'"></RoomMap>
+      <RoomMap :thislevel="'10'"></RoomMap>
+      <RoomMap :thislevel="'11'"></RoomMap>
+      <RoomMap :thislevel="'12'"></RoomMap>
+      <RoomMap :thislevel="'13'"></RoomMap>
+      <RoomMap :thislevel="'14'"></RoomMap>
+      <RoomMap :thislevel="'15'"></RoomMap>
+      <RoomMap :thislevel="'16'"></RoomMap>
+      <RoomMap :thislevel="'17'"></RoomMap>
+      <RoomMap :thislevel="'18'"></RoomMap>
+      <RoomMap :thislevel="'19'"></RoomMap>
+      <RoomMap :thislevel="'20'"></RoomMap>
+      <RoomMap :thislevel="'21'"></RoomMap>
+      <RoomMap :thislevel="'22'"></RoomMap>
+      <RoomMap :thislevel="'23'"></RoomMap>
+      <RoomMap :thislevel="'24'"></RoomMap>
+      <RoomMap :thislevel="'25'"></RoomMap>
+    </div>
+    <div class="right">
+      <el-tabs tab-position="right" style="height: 1000px;">
+        <el-tab-pane v-for="n in 25" :key="n" :label="n.toString()"></el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +44,21 @@ export default {
 }
 </script>
 
-<style></style>
+
+<style>
+.container {
+  /* display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between; */
+}
+
+.left {
+  /* flex: 0.7; */
+}
+
+.right {
+
+  /* flex: 0.3; */
+}
+</style>
