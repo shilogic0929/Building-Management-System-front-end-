ae9c4a73e9c6ab48d3b099cee6cdf6b32f6a9489
rainysong <songyuqwq@163.com>
2023-06-27 00:02:40 +0800

    fix visitor

--- edifice/src/layout/Components/SideBar.vue
+++ edifice/src/layout/Components/SideBar.vue
@@ -93,32 +93,102 @@
       </el-icon>
       <span class="sideBarFontSize">工作人员</span>
     </el-menu-item>
-    <el-menu-item v-if="type === '0'">
-      <el-icon>
-        <Message />
-      </el-icon>
-      <span class="sideBarFontSize">访客申请</span>
+    <el-menu-item index="/visitor" v-if="type === '0'">
+      <el-icon><Message /></el-icon>
+      <span class="sideBarFontSize" @click="dialogVisible = true">访客申请</span>
     </el-menu-item>
   </el-menu>
+  <el-dialog v-model="dialogVisible" title="访客申请" width="60%" draggable class="dlg" append-to-body="true">
+        <el-card class="input-card">
+          <div>申请表单: </div>
+          <div style="margin: 20px" />
+          <el-form :label-position="top" label-width="100px" :rules="rules" ref="input" style="max-width: 800px">
+            <el-form-item label="姓名">
+              <el-input v-model="input.user_name" />
+            </el-form-item>
+            <el-form-item label="身份证号码" prop="idNumber">
+              <el-input v-model="input.user_id" />
+            </el-form-item>
+            <el-form-item label="联系电话" prop="phone">
+              <el-input v-model="input.phone_num" />
+            </el-form-item>
+            <el-form-item label="访问时间" style="margin-top: 20px;">
+              <div class="block">
+                <el-date-picker v-model="input.visit_time" type="datetime" placeholder="选择日期和时间" value-format="YYYY-MM-DD HH:mm:ss"/>
+              </div>
+            </el-form-item>
+          </el-form>
+          <span class="dialog-footer" style="padding-left: 100px">
+            <el-button @click="dialogVisible = false">
+              取消
+              <el-icon>
+                <CircleClose />
+              </el-icon>
+            </el-button>
+            <el-button class="submit" size="middle" @click="submitInput(input)">
+              提交
+              <el-icon>
+                <Promotion />
+              </el-icon>
+            </el-button>
+          </span>
+        </el-card>
+        <el-card style="margin-top: 20px;!important" v-model="showActivities">
+          <span>申请处理进度</span>
+          <div class="block">
+            <el-timeline>
+              <el-timeline-item v-for="(activity, index) in activities" :key="index" :timestamp="activity.timestamp"
+                :color="activity.color" :hollow="activity.hollow">
+                {{ activity.content }}
+              </el-timeline-item>
+            </el-timeline>
+          </div>
+        </el-card>
+      </el-dialog>
 </template>
 
 <script>
-import {
-  Check,
-  CircleCheck,
-  Setting,
-  UserFilled,
-  Plus,
-  House,
-  Guide,
-  SwitchButton, User,
-  Message,
-} from '@element-plus/icons-vue'
+
 export default {
   data() {
     return {
       isCollapse: false,
       type: '',
+      dialogVisible: false,
+      input: {
+        user_name: "",
+        user_id: "",
+        phone_num: "",
+        visit_time: "",
+      },
+      rules: {
+        phone: [
+          { required: false },
+          // 这个只能验证手机号
+          // { pattern:/^0{0,1}(13[0-9]|15[7-9]|153|156|18[7-9])[0-9]{8}$/, message: "请输入合法手机号", trigger: "blur" }
+          { pattern: /^((0\d{2,3}-\d{7,8})|(1[3584]\d{9}))$/, message: "请输入合法手机号/电话号", trigger: "blur" }
+        ],
+        idNumber: [
+          { required: false },
+          { pattern: /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$)/, message: "请输入合法身份证号", trigger: "blur" }
+        ],
+      },
+      activities: [{
+        content: '申请时间',
+        timestamp: '',
+        color: '',
+        hollow: true
+      }, {
+        content: '通过审核',
+        timestamp: '',
+        color: '',
+        hollow: true
+      }, {
+        content: '访问时间',
+        timestamp: '',
+        hollow: true
+      }],
+      showActivities: false,
     }
   },
   computed: {
@@ -137,13 +207,74 @@ export default {
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    },
+    submitInput(input) {
+      if (input.user_name === '') {
+        this.$message("请输入您的姓名")
+        return
+      }
+      if (input.user_id === '') {
+        this.$message("请输入您的身份证号码")
+        return
+      }
+      if (input.phone_num === '') {
+        this.$message("请输入您的联系电话")
+        return
+      }
+      if (input.user_id === '') {
+        this.$message("请输入您的身份证号码")
+        return
+      }
+      if (input.visit_time === '') {
+        this.$message("请设置访问时间")
+        return
+      }
+      this.activities[0].timestamp = dateTime
+      this.activities[0].color = "#0bbd87"
+      this.activities[1].timestamp = dateTime
+      this.activities[1].color = "#0bbd87"
+      console.log(input.visit_time)
+      let timestamp = Date.parse(new Date(input.visit_time).toString());
+      console.log(timestamp)
+      let formData = new FormData()
+      formData.append('token', localStorage.getItem('token'))
+      formData.append('user_name', input.user_name)
+      formData.append('user_id', input.user_id)
+      formData.append('phone_num', input.phone_num)
+      formData.append('visit_time', timestamp)
+      this.$axios({
+        method: 'POST',
+        url: '/userApply',
+        data: //{
+          // 'token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImZmZkBnZy5jb20iLCJleHAiOjc3ODcxNTU4MjAsImVtYWlsIjoiZmZmQGdnLmNvbSJ9.65p8pwWLednWSEQlpGcU8Etvlwlf-nUD7WChWz6n0-I',
+          // 'user_name' : 'sxcsxc',
+          // 'user_id' : '220102200010101235',
+          // 'phone_num' : '18888888888',
+          // 'visit_time' : '1687585349'
+          //}
+          formData
+      }).then(res => {
+        if (res.status !== 200 && res.data.success == false) {
+          this.$message.error('提交失败：' + res.statusText);
+          return;
+        } else {
+          if (res.data.errno == 0) {
+            this.$message.success('提交成功')
+            this.activities[2].timestamp = input.visit_time
+            this.activities[2].color = "#0bbd87"
+          }
+          else
+            this.$message("您已经申请过")
+          setTimeout(() => {
+            this.dialogVisible = false;
+          }, 1000);
+        }
+      })
     }
   },
-
-
   created() {
     this.type = localStorage.getItem('type')
-    console.log(this.type)
+    console.log()
   }
 }
 </script>
