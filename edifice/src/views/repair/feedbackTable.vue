a16b8fd00baeac711c9daaf39f984a897e2f404d
shi logic <2041341499@qq.com>
2023-06-26 17:27:10 +0800

    latest

--- edifice/src/views/repair/feedbackTable.vue
+++ edifice/src/views/repair/feedbackTable.vue
@@ -84,7 +84,8 @@
             width="200"
             align="center">
         </el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="操作" 
+            align="center">
             <template v-slot="scope">
                 <el-button round type="info" 
                 size="small"
